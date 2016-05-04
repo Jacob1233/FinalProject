cc704fdca75573d753738e08ba847f0cf98dbb97
unknown <michaeluwaeke@ROOM316-11.student.rockhurst.int>
2016-05-04 14:05:19 -0500

    Changes

--- src/Course.java
+++ src/Course.java
@@ -3,10 +3,10 @@ import java.util.List;
 import java.util.UUID;
 
 public class Course {
-	public String courseName;
-	public UUID courseID;
-	public Teacher instructor;
-	public List<Student> roster = new ArrayList<Student>();
+	private String courseName;
+	private UUID courseID;
+	private Teacher instructor;
+	private List<Student> roster = new ArrayList<Student>();
 	
 	public Course(String courseName, Teacher instructor, List<Student> roster) {
 		this.courseName = courseName;
@@ -16,6 +16,15 @@ public class Course {
 	
 	private void makeID() {
 		courseID = UUID.randomUUID();
+	}
+	public String getCourseName(){
+		return courseName; 
+	}
+	public UUID getCourseID(){
+		return courseID;
+	}
+	public Teacher getinstructor(){
+		return instructor;
 	}
 
 }
